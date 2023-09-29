aba5d529f3397381725aa489070d5f69449f3ffe
raj8pancholi <56172158+raj8pancholi@users.noreply.github.com>
2023-09-29 08:48:18 +0530

    update

--- src/pages/website/merchant_register/elements/Store/BusinessFormHoursSection.js
+++ src/pages/website/merchant_register/elements/Store/BusinessFormHoursSection.js
@@ -1,19 +1,29 @@
 import React, {useState} from 'react'
 import WhatappInputbox from '../../../../../components/CommonComponent/WhatappInputbox';
 
-export default function BusinessFormHoursSection() {
+export default function BusinessFormHoursSection(props) {
+    const { daysState } = props; 
 
-    // Hours Script
-    const [showSundayBox, setShowSundayBox] = useState(false);
-    const [showMondayBox, setShowMondayBox] = useState(false);
-    const [showTuesdayBox, setShowTuesdayBox] = useState(false);
-    const [showWednesdayBox, setShowWednesdayBox] = useState(false);
-    const [showThursdayBox, setShowThursdayBox] = useState(false);
-    const [showFridayBox, setShowFridayBox] = useState(false);
-    const [showSaturdayBox, setShowSaturdayBox] = useState(false);
+    const sundayState = daysState.sunday.state;
+    const updateSunday = daysState.sunday.update; 
 
+    const mondayState = daysState.monday.state;
+    const updateMonday = daysState.monday.update;
 
+    const tuesdayState = daysState.tuesday.state;
+    const updateTuesday = daysState.tuesday.update;
 
+    const wednesdayState = daysState.wednesday.state;
+    const updateWednesday = daysState.wednesday.update;
+
+    const thursdayState = daysState.thursday.state;
+    const updateThursday = daysState.thursday.update;
+
+    const fridayState = daysState.friday.state;
+    const updateFriday = daysState.friday.update;
+
+    const saturdayState = daysState.saturday.state;
+    const updateSaturday = daysState.saturday.update;
 
 
 
@@ -26,24 +36,24 @@ export default function BusinessFormHoursSection() {
             <div className="col-12 day-box day-box1">
                 <span>Sunday </span>
                 <label className="switch">
-                    <input type="checkbox" onClick={()=> setShowSundayBox(!showSundayBox)}  />
+                    <input type="checkbox" checked={sundayState.status} onClick={()=> updateSunday(!(sundayState.status), sundayState.openTime, sundayState.closeTime)}  />
                     <span className="slider round"></span>
                 </label>
             </div>
 
-            {showSundayBox && (
+            {sundayState.status && (
                 <div className="col-12">
                 
                 <div className="hour-box hour-box1">
                     <div className="row">
                         <div className="col-5">
-                            <input type="time" className="form-control" name="sunday_from" placeholder="7:00 AM"/>
+                            <input type="time" className="form-control" name="sunday_from" placeholder="7:00 AM" onChange={(e)=> updateSunday(sundayState.status, e.target.value, sundayState.closeTime )}/>
                         </div>
                         <div className="col-2 text-center to-text">
                         To
                         </div>
                         <div className="col-5 pr-0">
-                            <input type="time" className="form-control" name="sunday_to" placeholder="10:30 PM"/>
+                            <input type="time" className="form-control" name="sunday_to" placeholder="10:30 PM"  onChange={(e)=> updateSunday(sundayState.status, sundayState.openTime, e.target.value)}/>
                         </div>
                     </div>
                 </div>
@@ -57,25 +67,25 @@ export default function BusinessFormHoursSection() {
         <div className="col-12 day-box day-box2">
             <span>Monday </span>
             <label className="switch">
-            <input type="checkbox" onClick={()=> setShowMondayBox(!showMondayBox)} />
+            <input type="checkbox" checked={mondayState.status} onClick={()=> updateMonday(!(mondayState.status), mondayState.openTime, mondayState.closeTime)}  />
             <span className="slider round"></span>
         </label>
 
         
         </div>
 
-        {showMondayBox && (
+        {mondayState.status && (
             <div className="col-12">
                 <div className="hour-box hour-box2">
                     <div className="row">
                         <div className="col-5">
-                            <input type="time" className="form-control" name="monday_from" placeholder="7:00 AM"/>
+                            <input type="time" className="form-control" name="monday_from" placeholder="7:00 AM" onChange={(e)=> updateMonday(mondayState.status, e.target.value, mondayState.closeTime )}/>
                         </div>
                         <div className="col-2 text-center to-text">
                         To
                         </div>
                         <div className="col-5 pr-0">
-                        <input type="time" className="form-control" name="monday_to" placeholder="10:30 PM"/>
+                        <input type="time" className="form-control" name="monday_to" placeholder="10:30 PM" onChange={(e)=> updateMonday(mondayState.status, e.target.value, mondayState.closeTime )}/>
                         </div>
                     </div>
                 </div>
@@ -91,13 +101,13 @@ export default function BusinessFormHoursSection() {
             <div className="col-12 day-box day-box3">
                 <span>Tuesday </span>
                 <label className="switch">
-                    <input type="checkbox" onClick={()=> setShowTuesdayBox(!showTuesdayBox)}/>
+                    <input type="checkbox"  checked={tuesdayState.status} onClick={()=> updateTuesday(!(tuesdayState.status), tuesdayState.openTime, tuesdayState.closeTime)}  />
                     <span className="slider round"></span>
                 </label>
 
             </div>
 
-            {showTuesdayBox && (
+            {tuesdayState.status && (
                 <div className="col-12">
                 <div className="hour-box hour-box3">
                     <div className="row">
@@ -125,14 +135,14 @@ export default function BusinessFormHoursSection() {
             <div className="col-12 day-box day-box4">
                 <span>Wednesday </span>
                 <label className="switch">
-                    <input type="checkbox" onClick={()=> setShowWednesdayBox(!showWednesdayBox)} />
+                    <input type="checkbox" checked={wednesdayState.status} onClick={()=> updateWednesday(!(wednesdayState.status), wednesdayState.openTime, wednesdayState.closeTime)}  />
                     <span className="slider round"></span>
                 </label>
 
             
             </div>
 
-            {showWednesdayBox && (
+            {wednesdayState.status && (
                 <div className="col-12">
                 <div className="hour-box hour-box4">
                     <div className="row">
@@ -158,12 +168,12 @@ export default function BusinessFormHoursSection() {
             <div className="col-12 day-box day-box5">
                 <span>Thursday </span>
                 <label className="switch">
-                    <input type="checkbox" onClick={()=> setShowThursdayBox(!showThursdayBox)}/>
+                    <input type="checkbox" checked={thursdayState.status} onClick={()=> updateThursday(!(thursdayState.status), thursdayState.openTime, thursdayState.closeTime)}  />
                     <span className="slider round"></span>
                 </label>
             </div>
 
-            {showThursdayBox && (
+            {thursdayState.status && (
                 <div className="col-12">
                 <div className="hour-box hour-box5">
                     <div className="row">
@@ -189,13 +199,13 @@ export default function BusinessFormHoursSection() {
             <div className="col-12 day-box day-box6">
                 <span>Friday </span>
                 <label className="switch">
-                    <input type="checkbox" onClick={()=> setShowFridayBox(!showFridayBox)}/>
+                    <input type="checkbox"  checked={fridayState.status} onClick={()=> updateFriday(!(fridayState.status), fridayState.openTime, fridayState.closeTime)}  />
                     <span className="slider round"></span>
                 </label>
             </div>
 
 
-            {showFridayBox && (
+            {fridayState.status && (
                 <div className="col-12">
                 <div className="hour-box hour-box6">
                     <div className="row">
@@ -223,13 +233,13 @@ export default function BusinessFormHoursSection() {
 
                 </span>
                 <label className="switch">
-                    <input type="checkbox" onClick={()=> setShowSaturdayBox(!showSaturdayBox)} value="false"/>
+                    <input type="checkbox"  checked={saturdayState.status} onClick={()=> updateSaturday(!(saturdayState.status), saturdayState.openTime, saturdayState.closeTime)}/>
                     <span className="slider round"></span>
                 </label>
             </div>
 
 
-            {showSaturdayBox && (
+            {saturdayState.status && (
                 <div className="col-12">
                 <div className="hour-box hour-box7">
                     <div className="row">
--- src/redux/slices/subCategorySlice.js
+++ src/redux/slices/subCategorySlice.js
@@ -1,53 +1,37 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import CategoryDataService from "../services/categoryServices";
 
+ 
 
-export const fetchSubcategory  = createAsyncThunk('subcategories/fetchSubcategory', async (categoryId, { rejectWithValue, fulfillWithValue })=> {
+export const fetchSubcategory = createAsyncThunk(
+  "subcategories/fetchCategories",
+  async (id) => {
+    const res = await CategoryDataService.getSubcategory(id);
+    return res.data;
+  }
+);
 
-    try {
-    
-      const response = await fetch(`http://3.108.250.128:3000/v1/merchant/subcategory/?categoryId=${categoryId}`);
-
-        if(response.ok) {
-            const data = await response.json();
-            return fulfillWithValue(data);
-        }else{
-            const data = await response.json();
-        return rejectWithValue(data);
-        }
-        
-
-
-    } catch (error) {
-        return rejectWithValue(error.message);
-    }
-});
 
 
 
 const subCategorySlice = createSlice({
   name: 'subcategories',
-  initialState: {
-    subcategories: [],
-    loading: false,
-    error: null,
-  },
+  initialState: { subcategories: [], loading: false, error: null, },
   reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchSubcategory.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(fetchSubcategory.fulfilled, (state, action) => {
+  extraReducers:{
+
+    [fetchSubcategory.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [fetchSubcategory.fulfilled]: (state, action) => {
         state.loading = false;
         state.subcategories = action.payload;
-      })
-      .addCase(fetchSubcategory.rejected, (state, action) => {
-        state.loading = false;
+    },
+    [fetchSubcategory.rejected]: (state, action) => {
+       state.loading = false;
         state.error = action.error.message;
-      });
+    },
   },
 });
 
-
-  
 export default subCategorySlice.reducer;
